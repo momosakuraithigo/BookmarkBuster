bdf9c1780643e5cc7c318c308c58447346be55de
momosakuraithigo <kyokoogawa435@gmail.com>
2024-11-27 23:18:34 +0900

    よく使うタグ候補を表示

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,10 +4,10 @@ class BookmarksController < ApplicationController
 
   def index
     @bookmarks = if params[:tag_name].present?
-      current_user.bookmarks.joins(:tags).where(tags: { name: params[:tag_name] })
-    else
-      current_user.bookmarks
-    end.page(params[:page]).per(10)
+                   current_user.bookmarks.joins(:tags).where(tags: { name: params[:tag_name] })
+                 else
+                   current_user.bookmarks
+                 end.page(params[:page]).per(10)
 
     # タグ一覧を取得（N+1問題を防ぐためincludes使用）
     @tags = current_user.bookmarks.includes(:tags).map(&:tags).flatten.uniq
@@ -19,25 +19,30 @@ class BookmarksController < ApplicationController
 
   def new
     @bookmark = Bookmark.new
+    @suggested_tags = fetch_frequent_tags
   end
 
   def create
     @bookmark = current_user.bookmarks.new(bookmark_params)
     if @bookmark.save
-      update_tags 
+      update_tags(@bookmark)
       redirect_to bookmarks_path, notice: 'ブックマークを追加しました。'
     else
-      render :new, status: :unprocessable_entity
+      @suggested_tags = fetch_frequent_tags
+      render :new
     end
   end
 
-  def edit; end
+  def edit
+    @suggested_tags = fetch_frequent_tags
+  end
 
   def update
     if @bookmark.update(bookmark_params)
-      update_tags
+      update_tags(@bookmark)
       redirect_to bookmarks_path, notice: 'ブックマークを更新しました。'
     else
+      @suggested_tags = fetch_frequent_tags
       render :edit, status: :unprocessable_entity
     end
   end
@@ -57,12 +62,19 @@ class BookmarksController < ApplicationController
     params.require(:bookmark).permit(:url, :title, :status)
   end
 
-  def update_tags
-    return unless params[:tag_names].present?
-    
-    tag_names = params[:tag_names].split(',').map(&:strip).reject(&:empty?)
-    @bookmark.tags = tag_names.map do |name|
-      Tag.find_or_create_by(name: name.strip)
-    end
+  # よく使われるタグを取得するメソッド
+  def fetch_frequent_tags
+    Tag.joins(:bookmark_tags)
+       .group("tags.id")
+       .order("COUNT(bookmark_tags.id) DESC")
+       .limit(10)
+  end
+
+  def update_tags(bookmark)
+    return unless params[:bookmark][:tag_names].present?
+
+    tag_names = params[:bookmark][:tag_names].split(',').map(&:strip).reject(&:empty?)
+    tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    bookmark.tags = tags
   end
 end
